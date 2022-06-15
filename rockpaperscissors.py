c094fb6e870ac99cfdc180cce5f23f8a8a9fa450
oagilliam <75933763+oagilliam@users.noreply.github.com>
2022-06-15 14:48:52 -0400

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -11,10 +11,10 @@ import sys
 while True:
     rps_dict = {1: 'rock', 2:'paper', 3:'scissors'}
     #This section checks to see if the player entered an option of rock, paper, or scissors. 
+    player = int(input('Select rock(1), paper(2), or scissors(3):'))
     for key in rps_dict:
-        player = int(input('Select rock(1), paper(2), or scissors(3):'))
         if player in rps_dict:
-            play_val = rps_dict[key]
+            play_val = rps_dict.get(key)
             break
         elif player not in rps_dict:
             print(type(player))
@@ -24,35 +24,13 @@ while True:
     #This section compares the player's option with the computer's option. 
     comp_rps = random.choice(list(rps_dict.values()))
     comp = comp_rps
-    print('test ' + comp)
     
-    if comp == 'rock':
-        if play_val == 'paper':
-            print('You win! Computer picked '+ comp)
-            break
-        elif play_val == 'scissors':
-            print('You lose! Computer picked '+ comp)
-            break
-        elif play_val == 'rock':
-            print('Game tied! You and computer picked ' + comp)
-            break
-    elif comp == 'paper':
-        if play_val == 'rock':
-            print('You lose! Computer picked '+ comp)
-            break
-        elif play_val == 'scissors':
-            print('You win! Computer picked '+ comp) 
-            break
-        elif play_val == 'paper':
-            print('Game tied! You and computer picked ' + comp)
-            break
-    elif comp == 'scissors':
-        if play_val == 'paper':
-            print('You lose! Computer picked '+ comp)
-            break
-        elif play_val == 'rock':
-            print('You win! Computer picked '+ comp)
-            break
-        elif play_val == 'scissors':
-            print('Game tied! You and computer picked ' + comp)
-            break
+    if comp == 'rock' and play_val == 'paper'or comp == 'paper' and play_val == 'scissors' or comp == 'scissors' and play_val == 'rock':
+        print('You win! Computer picked ' + comp)
+        break
+    elif comp == 'rock' and play_val == 'scissors' or comp == 'paper' and play_val == 'rock' or comp == 'scissors' and play_val == 'paper':
+        print('You lose! Computer picked ' + comp)
+        break
+    elif comp == 'rock' and play_val =='rock' or comp == 'paper' and play_val == 'paper' or comp == 'scissors' and play_val == 'scissors':
+        print('Game tied! Computer picked ' + comp)
+        break
