d4d0d33308b12f03c0d1a7f54961d2ae4f4b1793
oagilliam <75933763+oagilliam@users.noreply.github.com>
2022-06-14 21:03:33 -0400

    Create rockpaperscissors.py

---
+++ rockpaperscissors.py
@@ -0,0 +1,53 @@
+#Created by: Olivia G.
+#Completion date: 
+#Prompt: Two player will enter one option out of rock, paper, and scissors. The winning player will be displayed.
+#--Scissors beats paper.
+#--Paper beats rock.
+#--Rock beats scissors.
+
+import random
+import sys
+    
+while True:
+    rps_dict = {1: 'rock', 2:'paper', 3:'scissors'}
+    #This section checks to see if the player entered an option of rock, paper, or scissors. 
+    for key in rps_dict:
+        player = int(input('Select rock(1), paper(2), or scissors(3):'))
+        if player in rps_dict:
+            play_val = rps_dict[key]
+            break
+        elif player not in rps_dict:
+            print(type(player))
+            print('Error! Not an option!')
+            sys.exit()
+    
+    #This section compares the player's option with the computer's option. 
+    comp_rps = random.choice(list(rps_dict.values()))
+    comp = comp_rps
+    
+    if comp == 'rock':
+        if play_val == 'paper':
+            print('You win! Computer picked'+comp)
+        elif play_val == 'scissors':
+            print('You lose! Computer picked'+comp)
+        elif play_val == 'rock':
+            print('Game tied! You and computer picked' + comp)
+            sys.exit()
+    
+    if comp == 'paper':
+        if play_val == 'rock':
+            print('You win! Computer picked'+comp)
+        elif play_val == 'scissors':
+            print('You lose! Computer picked'+comp) 
+        elif play_val == 'paper':
+            print('Game tied! You and computer picked' + comp)
+            sys.exit()
+        
+    if comp == 'scissors':
+        if play_val == 'paper':
+            print('You win! Computer picked'+ comp)
+        elif play_val == 'rock':
+            print('You lose! Computer picked'+ comp)
+        elif play_val == 'scissors':
+            print('Game tied! You and computer picked' + comp)
+            sys.exit()
