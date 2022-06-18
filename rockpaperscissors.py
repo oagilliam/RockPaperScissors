66f31c3aae42a5cd1f41bca0d276bef31af09135
oagilliam <75933763+oagilliam@users.noreply.github.com>
2022-06-17 20:56:10 -0400

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -9,12 +9,13 @@ import random
 import sys
     
 while True:
-    rps_dict = {1: 'rock', 2:'paper', 3:'scissors'}
+    rps_dict = {'1': 'rock', '2':'paper', '3':'scissors'}
     #This section checks to see if the player entered an option of rock, paper, or scissors. 
-    player = int(input('Select rock(1), paper(2), or scissors(3):'))
+    player = input('Select rock(1), paper(2), or scissors(3):')
     for key in rps_dict:
         if player in rps_dict:
             play_val = rps_dict.get(key)
+            print('This is the value:' + play_val)
             break
         elif player not in rps_dict:
             print(type(player))
@@ -22,15 +23,21 @@ while True:
             sys.exit()
     
     #This section compares the player's option with the computer's option. 
+    r = 'rock'
+    p = 'paper'
+    s = 'scissors'
     comp_rps = random.choice(list(rps_dict.values()))
     comp = comp_rps
     
-    if comp == 'rock' and play_val == 'paper'or comp == 'paper' and play_val == 'scissors' or comp == 'scissors' and play_val == 'rock':
+    if comp == r and play_val == p or comp == p and play_val == s or comp == s and play_val == r:
+        print(play_val)
         print('You win! Computer picked ' + comp)
         break
-    elif comp == 'rock' and play_val == 'scissors' or comp == 'paper' and play_val == 'rock' or comp == 'scissors' and play_val == 'paper':
+    elif comp == r and play_val == s or comp == p and play_val == r or comp == s and play_val == p:
+        print(play_val)
         print('You lose! Computer picked ' + comp)
         break
-    elif comp == 'rock' and play_val =='rock' or comp == 'paper' and play_val == 'paper' or comp == 'scissors' and play_val == 'scissors':
+    elif comp == r and play_val == r or comp == p and play_val == p or comp == s and play_val == s:
+        print(play_val)
         print('Game tied! Computer picked ' + comp)
         break
